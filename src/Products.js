c0a13889e387dfa07cd24b4f8b3fd227d032a9cc
yash <yash_shukla@external.mckinsey.com>
2019-01-31 22:36:23 +0530

    added product file

---
+++ src/Products.js
@@ -0,0 +1,146 @@
+let products = [
+  {
+    name: "HONEY- THE LIQUID GOLD",
+    description:
+      "Honey is known as the liquid gold. It is not only high on nutrition, but it also alleviates allergies, acts as an instant energy drink, boosts memory, suppresses cough and cold, aids sleep, treats dandruff, treats wounds &amp; burns and much more.",
+    items: [
+      {
+        name: "Organic Natural Honey",
+        description: "The gift of nature in its purest form",
+        images: []
+      },
+      {
+        name: "Organic Jamun Honey",
+        description: "The goodness of the tropical fruits blended with honey."
+      },
+      {
+        name: "Organic Tulsi Honey",
+        description:
+          "Tulsi is known to be a medicinal herb itself. When blended with honey, it makes up for a warm and soothing combination.",
+        images: []
+      },
+      {
+        name: "Organic Ginger Honey",
+        description:
+          "Ginger and honey make up a healthy blend and an effective medicine for cough and cold.",
+        images: []
+      },
+      {
+        name: "Organic Cinnamon Honey",
+        description:
+          "The fragrant flavour of cinnamon comes together to make a warm and tasteful mixture for winter.",
+        images: []
+      }
+    ]
+  },
+  {
+    name: "FLAVOURED AND SEASONED ORGHANIC DRY FRUITS",
+    description: "",
+    items: [
+      {
+        name: "ORGANIC ROASTED ALMONDS",
+        description:
+          "Roasted in the Himalayan rock salt, organically grown almonds are rich inantioxidants and Vitamin E.",
+        healthBenefits: "It helps in lowering down the cholesterol levels.",
+        images: []
+      },
+      {
+        name: "ORGANIC ROASTED CASHEWS",
+        description:
+          "The roasted ‘chakhna,’ joins up with organic living to create Organic Roasted Cashews. Roasted in rock salt, cashews are rich in zinc, magnesium, copper and iron.",
+        healthBenefits:
+          "They help boost the immunity system and fights against cancer and promotes formation of Red Blood Cells.",
+        images: []
+      },
+      {
+        name: "ORGANIC DRY FRUIT TRAIL MIX",
+        description:
+          "A healthy mix of organic almonds, organic cashews, organic walnuts and organic raisins roasted in Himalayan rock salt resulting in a very unique &amp; delicious taste for anytime snacking.",
+        healthBenefits:
+          "For those who wish to manage weight and keep a watch on heart health the Organic Dry Fruit Trail Mix by MAA is great option. It also gives you a clear skin along with healthy hair since it is loaded with nutrients and is high in anti-oxidants.",
+        images: []
+      },
+      {
+        name: "CHOCOLATE CASHEWS",
+        description:
+          "The richness of cashew combined with delicious taste of chocolate, this product by MAA is the cherry on the dry fruits cake. Chocolate Cashews are rich in iron, magnesium, zinc and copper.",
+        healthBenefits:
+          "healthy cholesterol, boosts immune system, aids in curing cancer, promotes formation of RBCs",
+        images: []
+      }
+    ]
+  },
+  {
+    name: "NUTS AND SEEDS- UNIQUE BLEND OF TASTE AND HEALTH",
+    description: "",
+    items: [
+      {
+        name: "ORGANIC ROASTED FLAX SEEDS",
+        description:
+          "Organic flax seeds by MAA are roasted in Himalayan rock salt. The monotonous taste of flax seeds levels up with MAA.",
+        healthBenefits:
+          "Rich in high-quality proteins, they help control Cholesterol level and are loaded with nutrients. Rich in dietary fibre flax seeds is also known to lower blood pressure.",
+        images: []
+      },
+      {
+        name: "ROASTED PUMPKIN SEEDS",
+        description:
+          "The unprecedented flavour of pumpkin seeds comes together in the twist of being roasted and that too in Himalayan Rock Salt.",
+        healthBenefits:
+          "Health Benefits: Roasted Pumpkin Seeds are high in antioxidants, high in fibre, good for heart health, contain zinc for immune support, rich in plant-based omega-3 fats, good for prostate health, antidiabetic effects, beneficial in postmenopausal women, stress reliever &amp; provides restful sleep, anti-inflammatory benefits.",
+        images: []
+      },
+      {
+        name: "HEALTHY SEED MIX",
+        description:
+          "An absolutely new experience of all the healthy seeds sealed in one packet. A mixture of roasted edible seeds like pumpkin seeds, quinoa seeds, watermelon seeds, flax seeds, Healthy Seed Mix by MAA is a consolidated snack fulfilling the daily nutritional requirement.",
+        healthBenefits:
+          "High in protein and fibre, it is good for heart health.",
+        images: []
+      },
+      {
+        name: "ORGANIC ROASTED PEANUTS",
+        description:
+          "Purely organic peanuts sprinkled with rock salt are rich in proteins, minerals and Vitamin E.",
+        healthBenefits:
+          "It helps fight against nervous and heart diseases, prevents Alzheimer’s and nervous diseases.",
+        images: []
+      },
+      {
+        name: "RATLAMI ROASTED PEANUTS",
+        description:
+          "Indulge in binge snacking with MAA’s spicy and tangy ratlami roasted peanuts, which gives you an authentic feel of Ratlami aroma and spices right from the very first bite.",
+        healthBenefits: "",
+        images: []
+      },
+      {
+        name: "ACHARI ROASTED PEANUTS",
+        description:
+          "The tangy flavours of pickle with the goodness of peanuts rich in proteins, minerals and Vitamin E.",
+        healthBenefits: "",
+        images: []
+      },
+      {
+        name: "SWEET CHILLI ROASTED PEANUTS",
+        description:
+          "Explore the sweet and spicy combination of sweet chilli peanuts and savour the taste blended with health.",
+        healthBenefits: "",
+        images: []
+      },
+      {
+        name: "CHILLI LEMON ROASTED PEANUTS",
+        description:
+          "Chilli and lemon have always been the best combination when it comes to your love for spicy and tangy food.",
+        healthBenefits: "",
+        images: []
+      },
+      {
+        name: "CHILLI GARLIC ROASTED PEANUTS",
+        description:
+          "Chilli and garlic when blended together have always been a treat for our taste buds! So, MAA’s Chilli Garlic Peanuts provides you with all the health benefits combined with the best of taste.",
+        healthBenefits: "",
+        images: []
+      }
+    ]
+  }
+];
